bd4da102bdfb6c9e4e855f7a6d037c4de1862cea
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-04-27 13:23:07 -0400

    feat: add initial DKIM configuration to email sending

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -1,6 +1,11 @@
 import { IContact, IEmail } from '../schema/email';
 
-type IMCPersonalization = { to: IMCContact[] };
+type IMCPersonalization = {
+	to: IMCContact[];
+	dkim_domain?: string;
+	dkim_private_key?: string;
+	dkim_selector?: string;
+};
 type IMCContact = { email: string; name: string | undefined };
 type IMCContent = { type: string; value: string };
 
@@ -19,9 +24,15 @@ class Email {
 	 *
 	 * @param email
 	 */
-	static async send(email: IEmail) {
+	static async send(email: IEmail, env: Env) {
 		// convert email to IMCEmail (MailChannels Email)
 		const mcEmail: IMCEmail = Email.convertEmail(email);
+
+		if (env.DKIM_PRIVATE_KEY && env.DKIM_DOMAIN && env.DKIM_SELECTOR) {
+			mcEmail.personalizations[0].dkim_domain = env.DKIM_DOMAIN;
+			mcEmail.personalizations[0].dkim_private_key = env.DKIM_PRIVATE_KEY;
+			mcEmail.personalizations[0].dkim_selector = env.DKIM_SELECTOR
+		}
 
 		// send email through MailChannels
 		const resp = await fetch(
